70be07d7103dac8827bc5b236701db3e3c60fade
Pawel Kadluczka <moozzyk@gmail.com>
2019-02-24 17:47:16 -0800

    Adding CodeFirstFunctions verification suite

---
+++ CodeFirstFunctionsTest/CodeFirstFunctionsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CodeFirstFunctionsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodeFirstFunctionsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c636c52-0735-49d3-a678-dbf2846ef555")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
